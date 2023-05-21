c2190e1d7c7b045c52c3d131032b7590b4e322d8
Abhishek Turukmane <113658318+abhishekturukmane@users.noreply.github.com>
2023-05-21 18:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let contain=document.getElementsByClassName("container")[0]
+function show(){
+	
+	contain.innerHTML='';
+	let show=document.createElement("div")
+	show.id="line"
+	contain.append(show)
+	
+}
+function startAnimation() {
+
+	let line=document.getElementById("line")
+	line.className("line")
+	
+}
